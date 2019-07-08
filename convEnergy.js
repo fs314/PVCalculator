bde93a858fd243b0f36d0f33ec3ef7efeae1f30c
fs314 <flaminia.salustri@hotmail.it>
2019-07-08 11:17:59 +0200

    Added jquery to retrieve correct sap table based on area map click. Then selects right value on table using input values from orientation and slope.

--- convEnergy.js
+++ convEnergy.js
@@ -1,55 +1,96 @@
-function myFunction() {
- var form = document.getElementById("myForm");
-
- if(document.getElementById("selectedZone").innerHTML == "Zone 2"){
-
- var array1 = [];
- var array2 = [];
- var orientation = form.elements["Orientation"]
- var Orientation = orientation.value;
-
-
- var index = (Orientation/5)*4;
- var index1 = index + 1;
- var index2 = index + 2;
- var index3 = index + 3;
-
-console.log(index);
- var id = document.getElementById("id");
-
-  var myTable = document.getElementById("myTable");
-  var tr = myTable.getElementsByTagName("tr");
-  var input = document.getElementById("myInput");
-
-  var i;
-  for (i = 1; i < tr.length; i++) {
-    if(input.value == tr[i].getElementsByTagName("td")[0].textContent ) {
-
-	         for(var r = 1;r < 37; r++) {
-              array1 += tr[i].getElementsByTagName("td")[r].innerHTML ;
-             }
-			     if(array1[index] == 0) {
-				 id.innerHTML += array1[index1] + array1[index2] + array1[index3];
-         console.log(array1[index1] + array1[index2] + array1[index3]);
-				 }
-				 else {
-				     id.innerHTML += array1[index] + array1[index1] + array1[index2] + array1[index3];
-
-				 }
-
-	}
-  }
-
-}
-else {
-var g = document.getElementById("g");
-g.style.background = "yellow";
-
-}
-}
-
 
 function areaFunc(txt) {
    document.getElementById("selectedZone").innerHTML = txt;
 
 }
+
+$(document).ready(function () {
+
+
+      $('#zone1').click(function(){
+          $('#table').load( './zones/zone1.html' );
+      });
+      $('#zone2').click(function(){
+          $('#table').load( './zones/zone2.html' );
+      });
+      $('#zone3').click(function(){
+          $('#table').load( './zones/zone3.html' );
+      });
+      $('#zone4').click(function(){
+          $('#table').load( './zones/zone4.html' );
+      });
+      $('#zone5E').click(function(){
+          $('#table').load( './zones/zone5E.html' );
+      });
+      $('#zone6').click(function(){
+          $('#table').load( './zones/zone6.html' );
+      });
+      $('#zone7E').click(function(){
+          $('#table').load( './zones/zone7E.html' );
+      });
+      $('#zone8E').click(function(){
+          $('#table').load( './zones/zone8E.html' );
+      });
+      $('#zone9E').click(function(){
+          $('#table').load( './zones/zone9E.html' );
+      });
+      $('#zone10').click(function(){
+          $('#table').load( './zones/zone10.html' );
+      });
+      $('#zone11').click(function(){
+          $('#table').load( './zones/zone11.html' );
+      });
+      $('#zone12').click(function(){
+          $('#table').load( './zones/zone12.html' );
+      });
+      $('#zone13').click(function(){
+          $('#table').load( './zones/zone13.html' );
+      });
+      $('#zone14').click(function(){
+          $('#table').load( './zones/zone14.html' );
+      });
+      $('#zone15').click(function(){
+          $('#table').load( './zones/zone15.html' );
+      });
+      $('#zone16').click(function(){
+          $('#table').load( './zones/zone16.html' );
+      });
+      $('#zone17').click(function(){
+          $('#table').load( './zones/zone17.html' );
+      });
+      $('#zone18').click(function(){
+          $('#table').load( './zones/zone18.html' );
+      });
+      $('#zone19').click(function(){
+          $('#table').load( './zones/zone19.html' );
+      });
+      $('#zone20').click(function(){
+          $('#table').load( './zones/zone20.html' );
+      });
+      $('#zone21').click(function(){
+          $('#table').load( './zones/zone21.html' );
+      });
+      $('#zone5W').click(function(){
+          $('#table').load( './zones/zone5W.html' );
+      });
+      $('#zone7W').click(function(){
+          $('#table').load( './zones/zone7W.html' );
+      });
+      $('#zone8S').click(function(){
+          $('#table').load( './zones/zone8S.html' );
+      });
+      $('#zone9S').click(function(){
+          $('#table').load( './zones/zone9S.html' );
+      });
+
+      $('#calculate').click(function(){
+        var warning = "7";
+        var slope =  parseInt($('#slope').val()) + 2;
+        var orientation =  (parseInt($('#orientation').val())/5)+1;
+        if(orientation>19) {
+          warning = "orientation is not ideal to install PV, please contact us for a more detailed analysis"
+        }
+        var result = $('#myTable').find("tr:eq("+slope+")").find("td:eq("+orientation+")").text();
+        alert(result + " any warning " + warning);
+      });
+});
